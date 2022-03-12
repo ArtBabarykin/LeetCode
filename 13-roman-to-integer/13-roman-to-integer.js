e91708131ad2936a466f42c216e65ca2626454cf
ArtBabarykin <53874787+ArtBabarykin@users.noreply.github.com>
2022-03-12 15:39:21 +0300

    Time: 153 ms (78.87%), Space: 47.2 MB (48.43%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -3,32 +3,25 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    let numbers = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     let sum = 0;
-    let romanArr = s.split('');
-    for(let i = 0; i < romanArr.length; i++){
-        if(romanArr[i] === "M"){
-            sum += 1000;
-        } else if (romanArr[i] === 'D'){
-            sum += 500;
-        } else if(romanArr[i] === 'L'){
-            sum += 50;
-        } else if(romanArr[i] === 'V'){
-            sum += 5;
-        } else if(romanArr[i] === 'C' && (romanArr[i + 1] === 'D' || romanArr[i + 1] === 'M')){
-            sum -= 100; 
-        } else if(romanArr[i] === 'X' && (romanArr[i + 1] === 'L' || romanArr[i + 1] === 'C')){
-                  
-                  
-         sum -= 10;   
-         } else if(romanArr[i] === 'I' && (romanArr[i + 1] === 'V' || romanArr[i + 1] === 'X')){
-           sum -= 1;
-        } else if(romanArr[i] === 'C'){
-            sum += 100;
-        } else if(romanArr[i] === 'X'){
-            sum += 10;
-        } else if(romanArr[i] === 'I'){
-            sum += 1;
-        }
-    }       
+    
+    s.split('').forEach((letter, index, array) => {
+        if(numbers[array[index + 1]] === undefined || numbers[letter] >= numbers[array[index + 1]]){
+           sum += numbers[letter];
+           } else if(numbers[letter] < numbers[array[index + 1]]){
+                     sum -= numbers[letter];
+                     }
+    })
+   
+    
     return sum;
 };
