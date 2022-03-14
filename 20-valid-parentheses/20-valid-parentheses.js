aa5082abb8a32c70b924f03440eb865c19ebe6d4
ArtBabarykin <53874787+ArtBabarykin@users.noreply.github.com>
2022-03-14 22:49:05 +0300

    Time: 111 ms (24.26%), Space: 42.2 MB (54.00%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function (s) {
+  let legend = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+  
+    let arr = [];
+    for(let i = 0; i < s.length; i++){
+        if(s[i] in legend){
+           arr.push(s[i])
+           } else if(legend[arr.pop()] !== s[i]){
+                     return false;
+                     }
+    }
+    return arr.length ? false : true;
+};
