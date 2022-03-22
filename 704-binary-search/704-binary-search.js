d61a5d62e49828813bbb407813441b6c621c6ef2
ArtBabarykin <53874787+ArtBabarykin@users.noreply.github.com>
2022-03-22 21:54:42 +0300

    Time: 65 ms (89.04%), Space: 45.3 MB (24.68%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -4,5 +4,18 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    return nums.indexOf(target) 
+ let pivot,
+    left = 0,
+    right = nums.length - 1;
+
+  while (left <= right) {
+    pivot = Math.trunc(left + (right - left) / 2);
+    if (nums[pivot] === target) return pivot;
+    if (target < nums[pivot]) {
+      right = pivot - 1;
+    } else {
+      left = pivot + 1;
+    }
+  }
+  return -1;
 };
